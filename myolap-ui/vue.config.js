59503ed895fa05f66994d0b923a8f33611c35e70
wangshaoan <944060402@qq.com>
2020-03-19 12:03:06 +0800

    1

--- myolap-ui/vue.config.js
+++ myolap-ui/vue.config.js
@@ -66,7 +66,7 @@ module.exports = {
         }
       },*/
       '/myolap': {
-        target: 'http://localhost:8080', //请求本地 需要jeecg-boot后台项目
+        target: 'http://localhost:8080', //请求  1本地 需要jeecg-boot后台项目
         ws: false,
         changeOrigin: true
       },
